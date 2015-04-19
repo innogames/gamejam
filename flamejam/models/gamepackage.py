f28b10101ea193463685af7075a029dd8cc789a7
Niklas Grebe <niklas.grebe@innogames.com>
2015-04-19 17:59:24 +0200

    * Formatting...

--- flamejam/models/gamepackage.py
+++ flamejam/models/gamepackage.py
@@ -4,51 +4,53 @@ from flamejam import app, db
 from flask import Markup
 
 PACKAGE_TYPES = {
-    "web":           ("Web link (Flash etc.)", "Web"),
-    "linux":         ("Binaries: Linux 32/64-bit", "Linux"),
-    "linux32":       ("Binaries: Linux 32-bit", "Linux32"),
-    "linux64":       ("Binaries: Linux 64-bit", "Linux64"),
-    "windows":       ("Binaries: Windows", "Windows"),
-    "windows64":     ("Binaries: Windows 64-bit", "Windows64"),
-    "mac":           ("Binaries: MacOS Application", "MacOS"),
-    "source":        ("Source: package", "Source"),
-    "git":           ("Source: Git repository", "git"),
-    "svn":           ("Source: SVN repository", "svn"),
-    "hg":            ("Source: HG repository", "hg"),
-    "combi":         ("Combined package: Linux + Windows + Source (+ more, optional)", "Combined"),
-    "love":          ("Love package", ".love"),
-    "blender":       ("Blender file", ".blend"),
-    "unknown":       ("Other", "other")
+    "web": ("Web link (Flash etc.)", "Web"),
+    "linux": ("Binaries: Linux 32/64-bit", "Linux"),
+    "linux32": ("Binaries: Linux 32-bit", "Linux32"),
+    "linux64": ("Binaries: Linux 64-bit", "Linux64"),
+    "windows": ("Binaries: Windows", "Windows"),
+    "windows64": ("Binaries: Windows 64-bit", "Windows64"),
+    "mac": ("Binaries: MacOS Application", "MacOS"),
+    "source": ("Source: package", "Source"),
+    "git": ("Source: Git repository", "git"),
+    "svn": ("Source: SVN repository", "svn"),
+    "hg": ("Source: HG repository", "hg"),
+    "combi": ("Combined package: Linux + Windows + Source (+ more, optional)", "Combined"),
+    "love": ("Love package", ".love"),
+    "blender": ("Blender file", ".blend"),
+    "unknown": ("Other", "other")
 }
 
+
 class GamePackage(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(255))
     game_id = db.Column(db.Integer, db.ForeignKey("game.id"))
     type = db.Column(db.Enum(
-        "web",      # Flash, html5, js...
-        "linux",    # Linux binaries (e.g. *.tar.gz)
+        "web",  # Flash, html5, js...
+        "linux",  # Linux binaries (e.g. *.tar.gz)
         "linux32",  # Linux32 binaries (e.g. *.tar.gz)
         "linux64",  # Linux64 binaries (e.g. *.tar.gz)
         "windows",  # Windows binaries (e.g. *.zip, *.exe)
-        "windows64",# Windows64 binaries (e.g. *.zip, *.exe)
-        "mac",      # MacOS application packages
-        "combi",    # Linux + Windows + Source (and more, optional)
-        "love",     # Löve packages
+        "windows64",  # Windows64 binaries (e.g. *.zip, *.exe)
+        "mac",  # MacOS application packages
+        "combi",  # Linux + Windows + Source (and more, optional)
+        "love",  # Löve packages
         "blender",  # Blender save file (*.blend)
-        "source",   # Source package (e.g. *.zip or *.tar.gz)
-        "git",      # Version control repository: GIT
-        "svn",      # Version control repository: SVN
-        "hg",       # Version control repository: HG
+        "source",  # Source package (e.g. *.zip or *.tar.gz)
+        "git",  # Version control repository: GIT
+        "svn",  # Version control repository: SVN
+        "hg",  # Version control repository: HG
         "unknown"))
 
-    def __init__(self, game, url, type = "unknown"):
+    def __init__(self, game, url, type="unknown"):
         self.url = url
         self.type = type
         self.game = game
 
     def getLink(self):
-        return Markup('<a href="%s" target="_blank">%s</a>' % (self.url, GamePackage.typeString(self.type)))
+        return Markup('<a href="%s" style="color: #FFF;" target="_blank">%s</a>' % (self.url, GamePackage.typeString(
+            self.type)))
 
     def getLinkShort(self):
         return Markup('<a href="%s">%s</a>' % (self.url, GamePackage.typeStringShort(self.type)))
