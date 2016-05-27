f57da13bb676f787a411b39b5d97f36ef20ba0e3
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-05-27 10:53:51 +0200

    Server is running on localhost:5000

--- runserver.py
+++ runserver.py
@@ -4,5 +4,5 @@ from tornado.ioloop import IOLoop
 from flamejam import app
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(5000, address='127.0.0.1')
 IOLoop.instance().start()
