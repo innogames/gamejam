e02f4c075fbfe9192cf0c4c6b2c829a1b12636e4
Marco Behnke <marco.behnke@innogames.com>
2019-11-08 11:23:12 +0100

    proper first time jammer query

--- scripts/first_time_jammers.sh
+++ scripts/first_time_jammers.sh
@@ -2,7 +2,5 @@
 
 JAM_ID="$1"
 
-mysql gamejam -e "SELECT u.email
-FROM participation p left join user u on u.id = p.user_id
-WHERE p.jam_id = ${JAM_ID} and p.user_id in (select user_id from participation where jam_id = ${JAM_ID} and team_id in (select team_id from game where jam_id = ${JAM_ID}))
-GROUP by p.user_id limit 1;"
+mysql gamejam -e "SELECT u.email FROM user u LEFT JOIN participation p ON u.id = p.user_id WHERE p.jam_id = ${JAM_ID}
+    AND u.id NOT IN (SELECT u.id FROM user u LEFT JOIN participation p ON u.id = p.user_id WHERE p.jam_id != ${JAM_ID})"
